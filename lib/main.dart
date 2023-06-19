cf6887f7104b5436c5dba61e2437307f3ed204a1
alirezayoom <alirezayoom@gmail.com>
2023-06-19 23:27:03 +0430

    add heading

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color(0xff222222),
         appBar: AppBar(
           title: const Text("Real Estate"),
+          backgroundColor: Color(0xff111111),
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.all(10)),
+                  Text(
+                    "RENT A HOME",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Rental homes for everyone",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'Explore Apartments, Villas and Homes',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
