0a3f4e0092378270a99645a3577403d23b569124
alirezayoom <alirezayoom@gmail.com>
2023-06-20 20:03:46 +0430

    add filter_item

--- lib/components/filter_items.dart
+++ lib/components/filter_items.dart
@@ -5,6 +5,58 @@ class FilterItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red, height: 10, width: double.infinity);
+    return Container(
+        margin: const EdgeInsets.fromLTRB(10, 16, 10, 24),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "Filter",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: "Montserrat",
+                      fontWeight: FontWeight.bold),
+                ),
+                Icon(
+                  Icons.filter_list,
+                  color: Colors.white,
+                )
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xff444444),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'House',
+                    style: TextStyle(
+                        color: Colors.white, fontFamily: "Montserrat"),
+                  ),
+                ),
+                SizedBox(width: 10),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xff444444),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Apartment',
+                    style: TextStyle(
+                        color: Colors.white, fontFamily: "Montserrat"),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
